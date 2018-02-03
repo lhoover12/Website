ea6134a2c93a7ea4c62cb625955fab1e652960ab
lhoover12 <lukashoover@hotmail.com>
2018-02-03 00:33:54 -0600

    more clean up

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,5 @@
-import Panel from "./Panels";
 import Header from "./Header";
-import Picture from "./Picture";
 import Footer from "./Footer";
 
-export { Panel, Header, Picture, Footer };
-export default { Panel, Header, Picture, Footer };
+export { Header, Footer };
+export default { Header, Footer };

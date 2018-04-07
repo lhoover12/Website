b7dd220c380a01f5a0148eece042182c85e4c58a
lukas <lukashoover@hotmail.com>
2018-04-06 20:32:48 -0500

    Gimmme Gimme

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,8 @@ echo "$TRAVIS_PULL_REQUEST"
 echo "$TRAVIS_BRANCH"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Master baby!"
+        echo "Current build is a Pull"
+    echo "$TRAVIS_PULL_REQUEST"
 fi
 
 

5b4b1274407f61f805de16a3e12df47c211deb01
lukas <lukashoover@hotmail.com>
2018-04-06 20:20:53 -0500

    pr deploy testing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,10 @@
 echo "build script dude."
 echo "$TRAVIS_PULL_REQUEST" 
 echo "$TRAVIS_BRANCH"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Master baby!"
+fi
+
+
+

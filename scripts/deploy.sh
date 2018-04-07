93698c710cdfa7a86acaa12b963bcb242b8fa133
lukas <lukashoover@hotmail.com>
2018-04-07 10:24:28 -0500

    var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,11 @@ echo "$TRAVIS_PULL_REQUEST"
 echo "$TRAVIS_BRANCH"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-        echo "Current build is a Pull"
-    echo "$TRAVIS_PULL_REQUEST"
+    echo "Current build is a Pull Request "
+    PRNUM = "PR-$TRAVIS_PULL_REQUEST"
+    echo "$PRNUM"
 fi
+
 
 
 

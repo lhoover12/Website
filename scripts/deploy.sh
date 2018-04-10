bf1d344ffd3a666727a632320315f35320ecbfd7
lhoover12 <lukashoover@hotmail.com>
2018-04-09 19:12:11 -0500

    mmmmmm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,12 @@ echo "$TRAVIS_PULL_REQUEST"
 echo "$TRAVIS_BRANCH"
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Current build is a Pull Request "
+    aws configure set aws_access_key_id $AWS_ID
+    aws configure set aws_secret_access_key_id $AWS_SEC
     PRNUM="PR-$TRAVIS_PULL_REQUEST"
+    aws s3 sync ./build $S3_BUCKET/$PRNUM/ --metadata-directive REPLACE
+    echo "Current build is a Pull Request "
+    
     echo "$PRNUM"
 fi
 # if Pr deploy to s3 bucket/PR-(PRNUM)
